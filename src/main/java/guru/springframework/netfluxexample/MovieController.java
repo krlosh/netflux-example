a7196466c8d8e1c831eecd0096dd647abf4f411a
krlosh <krlosh@gmail.com>
2019-04-10 14:27:09 +0200

    Added movie controller

---
+++ src/main/java/guru/springframework/netfluxexample/MovieController.java
@@ -0,0 +1,37 @@
+package guru.springframework.netfluxexample;
+
+import guru.springframework.netfluxexample.domain.Movie;
+import guru.springframework.netfluxexample.domain.MovieEvent;
+import guru.springframework.netfluxexample.services.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    private MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping(value = "/{id}/events")
+    Flux<MovieEvent> streamMovieEvents(@PathVariable String id){
+        return this.movieService.events(id);
+    }
+
+    @GetMapping(value = "/{id}")
+    Mono<Movie> getMovieById(@PathVariable String id){
+        return this.movieService.getMovieById(id);
+    }
+
+    @GetMapping
+    Flux<Movie> getAllMovies(){
+        return this.movieService.getAllMovies();
+    }
+}
