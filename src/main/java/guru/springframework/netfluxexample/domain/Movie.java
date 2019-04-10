70b66b55296aadd495fdc8176f392ae400a005b8
krlosh <krlosh@gmail.com>
2019-04-10 14:03:24 +0200

    Loaded initial data using CommandLineRunner

--- src/main/java/guru/springframework/netfluxexample/domain/Movie.java
+++ src/main/java/guru/springframework/netfluxexample/domain/Movie.java
@@ -1,6 +1,6 @@
 package guru.springframework.netfluxexample.domain;
 
-import com.sun.istack.internal.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -9,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @Document
 @NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
 
     private String id;
